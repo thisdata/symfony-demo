068f12e6e051f4564bb530bd9213aab8976a482b
Nick Malcolm <nick@malcolm.net.nz>
2016-10-27 16:39:29 +1300

    Listen to login and logout events

---
+++ src/AppBundle/EventSubscriber/SecuritySubscriber.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * Listens to security related events like log-ins, failed logins, etc,
+ * and sends them to ThisData.
+ *
+ * Copyright (c) 2016 ThisData Ltd.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace AppBundle\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\AuthenticationEvents;
+use Symfony\Component\Security\Core\Event\AuthenticationFailureEvent;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use Symfony\Component\Security\Http\SecurityEvents;
+
+class SecuritySubscriber implements EventSubscriberInterface
+{
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            AuthenticationEvents::AUTHENTICATION_FAILURE => 'onAuthenticationFailure',
+            SecurityEvents::INTERACTIVE_LOGIN => 'onSecurityInteractiveLogin',
+        );
+    }
+
+    public function onAuthenticationFailure( AuthenticationFailureEvent $event )
+    {
+        error_log("onAuthenticationFailure");
+    }
+
+    public function onSecurityInteractiveLogin( InteractiveLoginEvent $event )
+    {
+        error_log("onSecurityInteractiveLogin");
+    }
+}
