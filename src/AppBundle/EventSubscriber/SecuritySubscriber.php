7ddd14fd8179a308e522d8c9ccf65fc6f260ef6b
Nick Malcolm <nick@malcolm.net.nz>
2016-10-28 13:51:33 +1300

    Use the ThisData package to track log in and failed log in events

--- src/AppBundle/EventSubscriber/SecuritySubscriber.php
+++ src/AppBundle/EventSubscriber/SecuritySubscriber.php
@@ -17,6 +17,8 @@ use Symfony\Component\Security\Core\Event\AuthenticationFailureEvent;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 use Symfony\Component\Security\Http\SecurityEvents;
+use ThisData\Api\ThisData;
+use ThisData\Api\Endpoint\EventsEndpoint;
 
 class SecuritySubscriber implements EventSubscriberInterface
 {
@@ -24,12 +26,15 @@ class SecuritySubscriber implements EventSubscriberInterface
     private $entityManager;
     private $tokenStorage;
     private $authenticationUtils;
+    private $thisData;
 
-    public function __construct(EntityManager $entityManager, TokenStorageInterface $tokenStorage, AuthenticationUtils $authenticationUtils)
+    public function __construct(EntityManager $entityManager, TokenStorageInterface $tokenStorage, AuthenticationUtils $authenticationUtils, $thisDataApiKey)
     {
         $this->entityManager = $entityManager;
         $this->tokenStorage = $tokenStorage;
         $this->authenticationUtils = $authenticationUtils;
+        $this->thisDataApiKey = $thisDataApiKey;
+        $this->thisData = ThisData::create($thisDataApiKey);
     }
 
     public static function getSubscribedEvents()
@@ -44,16 +49,41 @@ class SecuritySubscriber implements EventSubscriberInterface
     {
         $username = $this->authenticationUtils->getLastUsername();
         $existingUser = $this->entityManager->getRepository(User::class)->findOneBy(['username' => $username]);
+
         if ($existingUser) {
-           error_log("Log In Denied: Wrong password for User #" . $existingUser->getId()  . " (" . $existingUser->getEmail() . ")");
+            $userDetails = [
+                'id' =>             $existingUser->getId(),
+                'name' =>           $existingUser->getUsername(),
+                'email' =>          $existingUser->getEmail(),
+                'authenticated' =>  false
+            ];
         } else {
-           error_log("Log In Denied: User doesn't exist: " . $username);
+            $userDetails = [
+                'name' =>           $username,
+                'authenticated' =>  false
+            ];
         }
+
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $userAgent = $_SERVER['HTTP_USER_AGENT'];
+
+        $endpoint = $this->thisData->getEventsEndpoint();
+        $endpoint->trackEvent(EventsEndpoint::VERB_LOG_IN_DENIED, $ip, $userDetails, $userAgent);
     }
 
     public function onSecurityInteractiveLogin( InteractiveLoginEvent $event )
     {
         $user = $this->tokenStorage->getToken()->getUser();
-        error_log("Log In: User #" . $user->getId()  . " (" . $user->getEmail() . ")");
+
+        $userDetails = [
+            'id' =>             $user->getId(),
+            'name' =>           $user->getUsername(),
+            'email' =>          $user->getEmail()
+        ];
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $userAgent = $_SERVER['HTTP_USER_AGENT'];
+
+        $endpoint = $this->thisData->getEventsEndpoint();
+        $endpoint->trackEvent(EventsEndpoint::VERB_LOG_IN, $ip, $userDetails, $userAgent);
     }
 }
